5cb339c565c2b2f17b1376041af9a5311728da1f
Austin Spivey <aspivey@copleycg.com>
2020-03-03 00:29:50 -0500

    Hello World!

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,33 @@
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job = 'teacher';
+console.log(job);
+
+var glam = 'moo';
+console.log(glam);
+
+
+var job, isMarried;
+job = 'Professor';
+isMarried = true;
+console.log(job);
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'truck driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+
+
